1c1b49955553144bdbf7a04e1983c4e6cb51c5fa
Ji Chen <jaykchen@gmail.com>
2023-02-23 14:38:41 -0500

    init

---
+++ src/github-star-messenger.rs
@@ -0,0 +1,25 @@
+use github_flows::{listen_to_event, EventPayload};
+use slack_flows::send_message_to_channel;
+
+#[no_mangle]
+#[tokio::main(flavor = "current_thread")]
+pub async fn run() {
+    listen_to_event("jetjinser", "github-flows", vec!["issues"], handler).await;
+}
+
+async fn handler(payload: EventPayload) {
+    if let EventPayload::UnknownEvent(e) = payload {
+        let repo = e.get("repository").unwrap();
+
+        let full_name = repo["full_name"].as_str().unwrap();
+        let stargazers_count = repo["stargazers_count"].as_i64().unwrap();
+        let text = format!(
+            "Congratulations on your repository {} with {} stars.",
+            full_name, stargazers_count
+        );
+
+        if stargazers_count % 10 == 0 {
+            send_message_to_channel("jaykchen", "ik8", text)
+        }
+    }
+}
