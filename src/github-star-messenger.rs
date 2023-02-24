884661c0544e6dd806574d893af721c3fec44fa8
Ji Chen <jaykchen@gmail.com>
2023-02-23 22:00:01 -0500

    revert to listen

--- src/github-star-messenger.rs
+++ src/github-star-messenger.rs
@@ -5,36 +5,36 @@ use slack_flows::send_message_to_channel;
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() -> anyhow::Result<()> {
-    // listen_to_event("jaykchen", "vitesse-lite", vec!["star"], handler).await;
-    let octo = get_octo(None);
-    let repo = octo.repos("jaykchen", "vitesse-lite").get().await?;
+    listen_to_event("jaykchen", "vitesse-lite", vec!["star"], handler).await;
+    // let octo = get_octo(None);
+    // let repo = octo.repos("jaykchen", "vitesse-lite").get().await?;
 
-    let full_name = repo.full_name.unwrap();
-    let stargazers_count = repo.stargazers_count.unwrap();
-    let text = format!(
-        "Congratulations on your repository {} with {} stars.",
-        full_name, stargazers_count
-    );
-    send_message_to_channel("jaykchen", "ik8", text);
+    // let full_name = repo.full_name.unwrap();
+    // let stargazers_count = repo.stargazers_count.unwrap();
+    // let text = format!(
+    //     "Congratulations on your repository {} with {} stars.",
+    //     full_name, stargazers_count
+    // );
+    // send_message_to_channel("jaykchen", "ik8", text);
 
     Ok(())
 }
 
-// async fn handler(payload: EventPayload) {
-//     if let EventPayload::UnknownEvent(e) = payload {
-//         let repo = e.get("repository").unwrap();
-//         send_message_to_channel("jaykchen", "ik8", repo.to_string());
+async fn handler(payload: EventPayload) {
+    if let EventPayload::UnknownEvent(e) = payload {
+        let repo = e.get("repository").unwrap();
+        send_message_to_channel("jaykchen", "ik8", repo.to_string());
 
-//         let full_name = repo["full_name"].as_str().unwrap();
-//         let stargazers_count = repo["stargazers_count"].as_i64().unwrap();
-//         let text = format!(
-//             "Congratulations on your repository {} with {} stars.",
-//             full_name, stargazers_count
-//         );
-//         send_message_to_channel("jaykchen", "ik8", text.clone());
+        let full_name = repo["full_name"].as_str().unwrap();
+        let stargazers_count = repo["stargazers_count"].as_i64().unwrap();
+        let text = format!(
+            "Congratulations on your repository {} with {} stars.",
+            full_name, stargazers_count
+        );
+        send_message_to_channel("jaykchen", "ik8", text.clone());
 
-//         if stargazers_count % 10 == 0 {
-//             send_message_to_channel("jaykchen", "ik8", text)
-//         }
-//     }
-// }
+        if stargazers_count % 10 == 0 {
+            send_message_to_channel("jaykchen", "ik8", text)
+        }
+    }
+}
