f5a985e052511b8302f1ea93a3bc87b4a29c5e9d
Ji Chen <jaykchen@gmail.com>
2023-02-24 18:22:44 -0500

    send all payload

--- src/github-star-messenger.rs
+++ src/github-star-messenger.rs
@@ -22,19 +22,19 @@ pub async fn run() -> anyhow::Result<()> {
 
 async fn handler(payload: EventPayload) {
     if let EventPayload::UnknownEvent(e) = payload {
-        let repo = e.get("repository").unwrap();
-        send_message_to_channel("jaykchen", "ik8", repo.to_string());
+        // let repo = e.get("repository").unwrap();
+        send_message_to_channel("jaykchen", "ik8", e.to_string());
 
-        let full_name = repo["full_name"].as_str().unwrap();
-        let stargazers_count = repo["stargazers_count"].as_i64().unwrap();
-        let text = format!(
-            "Congratulations on your repository {} with {} stars.",
-            full_name, stargazers_count
-        );
-        send_message_to_channel("jaykchen", "ik8", text.clone());
+        // let full_name = repo["full_name"].as_str().unwrap();
+        // let stargazers_count = repo["stargazers_count"].as_i64().unwrap();
+        // let text = format!(
+        //     "Congratulations on your repository {} with {} stars.",
+        //     full_name, stargazers_count
+        // );
+        // send_message_to_channel("jaykchen", "ik8", text.clone());
 
-        if stargazers_count % 10 == 0 {
-            send_message_to_channel("jaykchen", "ik8", text)
-        }
+        // if stargazers_count % 10 == 0 {
+        //     send_message_to_channel("jaykchen", "ik8", text)
+        // }
     }
 }
