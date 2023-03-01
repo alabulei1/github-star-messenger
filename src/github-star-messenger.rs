27237128413377bff1fd3a6766d1e5e022a7f54b
Ji Chen <jaykchen@gmail.com>
2023-02-28 20:12:51 -0500

    send slack message and email

--- src/github-star-messenger.rs
+++ src/github-star-messenger.rs
@@ -1,5 +1,6 @@
 use anyhow::{Error, Result};
 use github_flows::get_octo;
+use sendgrid_flows::{send_email, Email};
 use slack_flows::send_message_to_channel;
 
 #[no_mangle]
@@ -11,9 +12,17 @@ pub async fn run() -> anyhow::Result<()> {
 
     let stargazers_count = repo.stargazers_count.unwrap_or(0);
     send_message_to_channel("ik8", "general", stargazers_count.to_string());
+    let text = format!("You have been rewarded {stargazers_count} stars!");
 
     if stargazers_count % 10 == 0 {
-        send_message_to_channel("jaykchen", "ik8", stargazers_count.to_string());
+        send_message_to_channel("jaykchen", "ik8", text.clone());
+        let email = Email {
+            to: vec![String::from("jaykchen@gmail.com")],
+            subject: String::from("Hi"),
+            content: text,
+        };
+        send_email("jaykchen@gmail.com", &email).expect("failed to send email");
     }
+
     Ok(())
 }
