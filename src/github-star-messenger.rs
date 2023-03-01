8156adeb3b87cd954eb57f1b2d05846ab621e0fe
Ji Chen <jaykchen@gmail.com>
2023-02-28 21:17:28 -0500

    register to platform with listen

--- src/github-star-messenger.rs
+++ src/github-star-messenger.rs
@@ -1,11 +1,13 @@
 use anyhow::{Error, Result};
-use github_flows::get_octo;
+use github_flows::{get_octo, listen_to_event, EventPayload};
 use sendgrid_flows::{send_email, Email};
 use slack_flows::send_message_to_channel;
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() -> anyhow::Result<()> {
+    listen_to_event("jaykchen", "vitesse-lite", vec!["star"], handler).await;
+
     let octocrab = get_octo(None);
 
     let repo = octocrab.repos("jaykchen", "vitesse-lite").get().await?;
@@ -25,4 +27,33 @@ pub async fn run() -> anyhow::Result<()> {
     }
 
     Ok(())
+}
+
+async fn handler(payload: EventPayload) {
+    // let mut title: String = "no title found".to_string();
+    // let mut merged: bool = false;
+    // let mut html_url: String = "no html_url found".to_string();
+    // let mut user: String = "".to_string();
+
+    // if let EventPayload::PullRequestEvent(e) = payload {
+    //     let pr = e.pull_request;
+    //     title = pr.title.expect("no title");
+    //     html_url = pr.html_url.expect("no html_url field").to_string();
+    //     user = pr.user.expect("user not found").login;
+    //     merged = match pr.merge_commit_sha {
+    //         Some(_sha) => true,
+    //         None => false,
+    //     };
+    // }
+
+    // get_email(&user).await;
+
+    // match get_email(&user).await {
+    //     None => {}
+    //     Some(email) => {
+    // let merged_str = if merged { "merged" } else { "null" };
+    // let text = format!("title: {title}\n html_url: {html_url}\n user: {user}\n, email: email\n merged: {merged_str}");
+    // send_message_to_channel("ik8", "general", text);
+    // }
+    // }
 }
