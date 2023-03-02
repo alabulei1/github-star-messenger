079d7a5c7f27b12c11d4faa6a07e08deca7226af
Ji Chen <jaykchen@gmail.com>
2023-03-02 09:20:06 -0500

    get stars count from inside handler

--- src/github-star-messenger.rs
+++ src/github-star-messenger.rs
@@ -1,4 +1,4 @@
-use github_flows::{get_octo, listen_to_event, EventPayload};
+use github_flows::{listen_to_event, EventPayload};
 use sendgrid_flows::{send_email, Email};
 
 #[no_mangle]
@@ -10,21 +10,10 @@ pub async fn run() -> anyhow::Result<()> {
 }
 
 async fn handler(payload: EventPayload) {
-    let your_github_login: &str = "jaykchen";
     let your_repo_name: &str = "vitesse-lite";
 
-    // let octocrab = get_octo(None);
-
-    // let repo = octocrab
-    //     .repos(your_github_login, your_repo_name)
-    //     .get()
-    //     .await
-    //     .expect("repo not obtained");
-
-    // let stargazers_count = repo.stargazers_count.unwrap_or(0);
     if let EventPayload::UnknownEvent(e) = payload {
-        let repo = e.get("repository").expect("repo not obtained");
-        let stargazers_count = repo["stargazers_count"].as_i64().unwrap_or(0);
+        let stargazers_count = e["repository"]["stargazers_count"].as_i64().unwrap_or(-1);
 
         let text = format!(
             "Congratulations on your repository {your_repo_name} with {stargazers_count} stars."
