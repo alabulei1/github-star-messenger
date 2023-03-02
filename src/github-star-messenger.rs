fb65ba38567d9c584c66e8e584008ef344871a7b
Ji Chen <jaykchen@gmail.com>
2023-03-01 20:19:24 -0500

    get stargazers_count from UnknownEvent

--- src/github-star-messenger.rs
+++ src/github-star-messenger.rs
@@ -4,37 +4,39 @@ use sendgrid_flows::{send_email, Email};
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() -> anyhow::Result<()> {
-    let your_github_login: &str = "jaykchen";
-    let your_repo_name: &str = "vitesse-lite";
-
-    listen_to_event(your_github_login, your_repo_name, vec!["star"], handler).await;
-
-    let octocrab = get_octo(None);
-
-    let repo = octocrab
-        .repos(your_github_login, your_repo_name)
-        .get()
-        .await?;
-
-    let stargazers_count = repo.stargazers_count.unwrap_or(0);
-
-    let text = format!(
-        "Congratulations on your repository {your_repo_name} with {stargazers_count} stars."
-    );
-
-    if stargazers_count % 10 == 0 {
-        let email = Email {
-            to: vec![String::from("jaykchen@gmail.com")],
-            subject: String::from("Hi"),
-            content: text,
-        };
-        send_email("jaykchen@gmail.com", &email).expect("failed to send email");
-    }
+    listen_to_event("jaykchen", "vitesse-lite", vec!["star"], handler).await;
 
     Ok(())
 }
 
 async fn handler(payload: EventPayload) {
-    let text = format!("{:?}", payload);
-    println!("{text}");
+    let your_github_login: &str = "jaykchen";
+    let your_repo_name: &str = "vitesse-lite";
+
+    // let octocrab = get_octo(None);
+
+    // let repo = octocrab
+    //     .repos(your_github_login, your_repo_name)
+    //     .get()
+    //     .await
+    //     .expect("repo not obtained");
+
+    // let stargazers_count = repo.stargazers_count.unwrap_or(0);
+    if let EventPayload::UnknownEvent(e) = payload {
+        let repo = e.get("repository").expect("repo not obtained");
+        let stargazers_count = repo["stargazers_count"].as_i64().unwrap_or(0);
+
+        let text = format!(
+            "Congratulations on your repository {your_repo_name} with {stargazers_count} stars."
+        );
+
+        if stargazers_count % 10 == 0 {
+            let email = Email {
+                to: vec![String::from("jaykchen@gmail.com")],
+                subject: String::from("Hi"),
+                content: text,
+            };
+            send_email("jaykchen@gmail.com", &email).expect("failed to send email");
+        }
+    }
 }
